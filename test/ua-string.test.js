cba6ea92004eda520482371156bd9fd4fc46534b
WindomZ <git.windomz@gmail.com>
2017-03-17 13:04:46 +0800

    add test units

---
+++ test/ua-string.test.js
@@ -0,0 +1,48 @@
+/**
+ * Created by WindomZ on 17-3-17.
+ */
+const test = require('ava');
+
+const fetch = require('../lib/ua-fetch');
+
+// test.serial('test', async(t) => {
+//   let r = '(Dillo/2.0), (Dillo/0.8.6-i18n-misc)';
+//   t.regex(r, /^\(Dillo\/.+\),\s\(Dillo\/.+\)/);
+//   t.pass();
+// });
+
+test.serial('Chrome -l 1', async(t) => {
+  await fetch({
+    limit: 1,
+    browser: 'Chrome',
+  }).then(list => {
+    let r = list.toString();
+    t.regex(r, /^"Mozilla.+"/);
+    t.pass();
+  }).catch(err => t.fail(err));
+});
+
+test.serial(`Dillo -l 2 -f '{(*)}'`, async(t) => {
+  await fetch({
+    format: '{(*)}',
+    limit: 2,
+    browser: 'Dillo',
+  }).then(list => {
+    let r = list.toString();
+    t.regex(r, /^{\(Dillo\/.+\)},\s{\(Dillo\/.+\)}/);
+    t.pass();
+  }).catch(err => t.fail(err));
+});
+
+test.serial(`Dillo -l 2 -f '(*)' -s '$$'`, async(t) => {
+  await fetch({
+    format: '(*)',
+    separator: '$$',
+    limit: 2,
+    browser: 'Dillo',
+  }).then(list => {
+    let r = list.toString();
+    t.regex(r, /^\(Dillo\/.+\)\$\$\(Dillo\/.+\)/);
+    t.pass();
+  }).catch(err => t.fail(err));
+});
