861de8aa8ea62ea2a45bfe27760a8a7fdd44f3d0
WindomZ <git.windomz@gmail.com>
2017-03-17 13:27:53 +0800

    fix regex test

--- test/regex.test.js
+++ test/regex.test.js
@@ -3,7 +3,7 @@
  */
 const test = require('ava');
 
-test.serial('test', async(t) => {
+test.serial('test', t => {
   let r = '(Dillo/2.0), (Dillo/0.8.6-i18n-misc)';
   t.regex(r, /^\(Dillo\/.+\),\s\(Dillo\/.+\)/);
   t.pass();
