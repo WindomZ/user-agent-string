10021cc229b8a4fac8fdb79bf7b8bd9372bba596
WindomZ <git.windomz@gmail.com>
2017-03-18 08:50:41 +0800

    add test for ua browser

---
+++ test/ua-browser.test.js
@@ -0,0 +1,24 @@
+/**
+ * Created by WindomZ on 17-3-18.
+ */
+const test = require('ava');
+
+const fetch = require('../lib/ua-browser');
+
+test.serial('browser search se', async(t) => {
+  await fetch({
+    search: 'se',
+  }).then(list => {
+    list.forEach(l => t.regex(l.toLowerCase(), /se/));
+    t.pass();
+  }).catch(err => t.fail(err));
+});
+
+test.serial('browser search di', async(t) => {
+  await fetch({
+    search: 'di',
+  }).then(list => {
+    list.forEach(l => t.regex(l.toLowerCase(), /di/));
+    t.pass();
+  }).catch(err => t.fail(err));
+});
