fd53a9d8d6ed9c8978ad268c75f016145eee9897
WindomZ <git.windomz@gmail.com>
2017-03-27 08:11:45 +0800

    style: support js standard style

--- test/regex.test.js
+++ test/regex.test.js
@@ -1,10 +1,10 @@
 /**
  * Created by WindomZ on 17-3-17.
  */
-const test = require('ava');
+const test = require('ava')
 
 test.serial('test', t => {
-  let r = '(Dillo/2.0), (Dillo/0.8.6-i18n-misc)';
-  t.regex(r, /^\(Dillo\/.+\),\s\(Dillo\/.+\)/);
-  t.pass();
-});
+  let r = '(Dillo/2.0), (Dillo/0.8.6-i18n-misc)'
+  t.regex(r, /^\(Dillo\/.+\),\s\(Dillo\/.+\)/)
+  t.pass()
+})
--- test/ua-fetch.test.js
+++ test/ua-fetch.test.js
@@ -1,42 +1,42 @@
 /**
  * Created by WindomZ on 17-3-17.
  */
-const test = require('ava');
+const test = require('ava')
 
-const fetch = require('../lib/ua-fetch');
+const fetch = require('../lib/ua-fetch')
 
 test.serial('Chrome -l 1', async(t) => {
   await fetch({
     limit: 1,
-    browser: 'Chrome',
+    browser: 'Chrome'
   }).then(list => {
-    let r = list.toString();
-    t.regex(r, /^"Mozilla.+"/);
-    t.pass();
-  }).catch(err => t.fail(err));
-});
+    let r = list.toString()
+    t.regex(r, /^"Mozilla.+"/)
+    t.pass()
+  }).catch(err => t.fail(err))
+})
 
 test.serial(`Dillo -l 2 -f '{(*)}'`, async(t) => {
   await fetch({
     format: '{(*)}',
     limit: 2,
-    browser: 'Dillo',
+    browser: 'Dillo'
   }).then(list => {
-    let r = list.toString();
-    t.regex(r, /^{\(Dillo\/.+\)},\s{\(Dillo\/.+\)}/);
-    t.pass();
-  }).catch(err => t.fail(err));
-});
+    let r = list.toString()
+    t.regex(r, /^{\(Dillo\/.+\)},\s{\(Dillo\/.+\)}/)
+    t.pass()
+  }).catch(err => t.fail(err))
+})
 
 test.serial(`Dillo -l 2 -f '(*)' -s '$$'`, async(t) => {
   await fetch({
     format: '(*)',
     separator: '$$',
     limit: 2,
-    browser: 'Dillo',
+    browser: 'Dillo'
   }).then(list => {
-    let r = list.toString();
-    t.regex(r, /^\(Dillo\/.+\)\$\$\(Dillo\/.+\)/);
-    t.pass();
-  }).catch(err => t.fail(err));
-});
+    let r = list.toString()
+    t.regex(r, /^\(Dillo\/.+\)\$\$\(Dillo\/.+\)/)
+    t.pass()
+  }).catch(err => t.fail(err))
+})
